7d304f78d1b9abcf03ed2e99c1beed430e29f711
Pedro Dantas <pedro.dantas@gmail.com>
2019-11-19 18:51:49 -0300

    Initial commit

---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+/*Controller é um objeto que manipula as solicitações HTTP (Via POSTMAN). Ele pode retornar uma lista de contas ou uma única conta através do ID.
+Além das operações da CRUD*/
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class meudeusdoceufuncionaController : ControllerBase
+    {
+        // GET api/agencia
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Isso", "Funciona" };
+        }
+
+        // GET api/agencia/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/agencia
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+         
+        }
+
+        // PUT api/agencia/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/agencia/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ WebApplication/Repositores/MyConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication.Repositores
+{
+    public class MyConfig
+    {
+        /*
+        *  Data Source:	Identifica o servidor e pode ser a máquina local (localhost), um dns ou um endereço IP.
+        *  Initial Catalog:	O nome do banco de dados
+        *  Integrated Security:	Defina para SSPI para fazer a conexão usando a autenticação do WIndows.(Usar Integrated Security é seguro somente quando você esta em uma máquina fora da rede)
+        *  User ID:	Nome do usuário definido no SQL Server. 
+        *  Password:	Senha do usuário definida no SQL Server.
+        */
+
+        //String de conexão
+        private string ConnectionString = "Data Source=localhost,1433;Initial Catalog=ProjetoPai;Integrated Security=False;User Id=sa;Password=sa";
+
+        private void CreateCommand()
+        {
+            //Cria uma instância do objeto Connection em memória com um unico construtor do tipo string
+            SqlConnection SQLConnection = new SqlConnection(ConnectionString);
+            try
+            {
+                //Abre a conexão
+                SQLConnection.Open();
+
+                //Passa a conexão para o objeto command (responsável por executar comandos contra o banco de dados)
+                SqlCommand command = new SqlCommand("select * from Clientes", SQLConnection);
+
+                /* Para criar um objeto DataReader usamos o método ExecuteReader de um objeto Command.
+                 * ExecuteReader executa a consulta e retorna um objeto SqlDataReader;
+                 * Objeto DataReader é uma maneira de ler os dados retornados pelos objetos Command, 
+                 * ele permite acessar e percorrer os registros no modo de somente leitura.
+                 */
+                SqlDataReader ReadQuery = command.ExecuteReader();
+
+                //Percorre e lê a primeira coluna de cada linha do conjunto de registros 
+                //Pode pecorrer por nome de coluna também ReadQuery[Clientes] por exemplo
+                while (ReadQuery.Read())
+                {
+                    Console.WriteLine(ReadQuery[0]);
+
+                    //Fecha o Reader e a conexão
+                    if (ReadQuery == null && SQLConnection == null)
+                    {
+                        ReadQuery.Close();
+                        SQLConnection.Close();
+                    }
+                }
+        }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Erro resultado: " + ex);
+            }
+
+        }
+
+    }
+}
